fb36d732347e7f59bf9fa378b11cec000efc2547
Jake Lee <jake.lee@fdmgroup.com>
2021-09-01 13:11:52 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
+import numpy as np
+
 def main():
     #write your code below this line
+    num1 = int(input())
+    num2 = int(input())
+    print(int(np.sqrt(num1 + num2)))
 
 if __name__ == '__main__':
     main()
